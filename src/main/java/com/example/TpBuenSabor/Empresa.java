22860b4db811fb6655c9a431e5e319de6c2767a0
lauti2311 <lautaro_ismael2010@hotmail.com>
2024-04-15 14:52:36 -0300

    Ejemplo de conexion de empresa y sucursalEmpresa

--- src/main/java/com/example/TpBuenSabor/Empresa.java
+++ src/main/java/com/example/TpBuenSabor/Empresa.java
@@ -1,16 +1,27 @@
 package com.example.TpBuenSabor;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity
 public class Empresa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String nombre;
     private String razonSocial;
     private int cuil;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "empresa_id")
+    private List<SucursalEmpresa> sucursales;
 }
--- src/main/java/com/example/TpBuenSabor/SucursalEmpresa.java
+++ src/main/java/com/example/TpBuenSabor/SucursalEmpresa.java
@@ -1,5 +1,9 @@
 package com.example.TpBuenSabor;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +13,11 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity
 public class SucursalEmpresa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String nombre;
     private String horarioApertura;
     private String horarioCierre;
